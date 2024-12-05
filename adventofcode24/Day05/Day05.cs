c7cf4ff3ebfeba1f44b2a451f15f85e91a9a9ba1
NewPing <jan-roth1998@web.de>
2024-12-05 16:49:03 +0100

    setup day05

---
+++ adventofcode24/Day05/Day05.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode24.Day05
+{
+    internal class Day05
+    {
+        public Day05()
+        {
+            var input = File.ReadAllLines(@"day05\input1.txt").ToList();
+            Console.WriteLine(part1(input));
+        }
+
+        private int part1(List<string> input)
+        {
+            return 0;
+        }
+    }
+}
--- adventofcode24/Program.cs
+++ adventofcode24/Program.cs
@@ -2,10 +2,12 @@
 using adventofcode24.Day02;
 using adventofcode24.Day03;
 using adventofcode24.Day04;
+using adventofcode24.Day05;
 using adventofcode24.Day1;
 
 Console.WriteLine("Hello, World!");
 //new Day01();
 //new Day02();
 //new Day03();
-new Day04();
+//new Day04();
+new Day05();
